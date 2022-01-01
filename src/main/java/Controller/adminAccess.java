228bbf4e27668b26f72673055ff3aeec5a2f20f4
proselin <72499558+proselin@users.noreply.github.com>
2022-01-02 06:41:24 +0700

    Upload 1

---
+++ src/main/java/Controller/adminAccess.java
@@ -0,0 +1,144 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+package Controller;
+
+import Entity.User;
+import Model.user_Model;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author quoch
+ */
+@WebFilter(filterName = "adminAccess", urlPatterns = {"/admin/*"})
+public class adminAccess implements Filter {
+
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+
+    public adminAccess() {
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession();
+        Object userIDprepare = session.getAttribute("use");
+        if (userIDprepare != null) {
+            String userid = userIDprepare.toString();
+            user_Model um = new user_Model();
+            User us = um.search_User_Data(userid);
+            if (!us.getRoleID().equals("Admin012") && !us.getRoleID().equals("Staff012")) {
+                response.sendRedirect("../404page.html");
+            } else {
+                chain.doFilter(req, res);
+            }
+        } else {
+            response.sendRedirect("../404page.html");
+        }
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     * @return 
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * Init method for this filter
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("adminAccess:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("adminAccess()");
+        }
+        StringBuffer sb = new StringBuffer("adminAccess(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);
+    }
+
+}
